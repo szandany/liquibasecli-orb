901eb59ef8588a16eb2f7250820d110ced5e3093
szandany <szandany@liquibase.com>
2022-07-25 11:36:20 -0500

    corrected docker image name to liquibase from dockerhub

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" status --verbose
-/liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" update
+/liquibase/liquibase --version
+# /liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" status --verbose
+# /liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" update
