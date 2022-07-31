9795b0c2c3aef713b72690aa32a1036e51b165da
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-07-31 18:01:08 -0500

    fixed update command flags issues

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -3,4 +3,5 @@ export JAVA_OPTS=$JAVA_OPTS
 LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$PARAM_URL --changeLogFile=$PARAM_CHANGELOG"
 /liquibase/liquibase --version
 /liquibase/liquibase $LB_PARAMS status --verbose
-/liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" update
+/liquibase/liquibase $LB_PARAMS update
+/liquibase/liquibase $LB_PARAMS history
