a6af060308f67d6564cbfd5f6c88a71f9f2eadf0
Tsvi Zandany  <tsvi@cci-tsvizandany-4mxcc.lan>
2022-07-31 17:54:06 -0500

    added update command default parameters

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export JAVA_OPTS=$JAVA_OPTS
+LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$PARAM_URL --changeLogFile=$PARAM_CHANGELOG"
 /liquibase/liquibase --version
-# /liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" status --verbose
-# /liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" update
+/liquibase/liquibase $LB_PARAMS status --verbose
+/liquibase/liquibase --changeLogFile="${PARAM_CHANGELOG}" update
