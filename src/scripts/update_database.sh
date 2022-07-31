ea4a9afd021dd07f624deef54c39a7a6593fefd0
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-07-31 18:30:14 -0500

    testing with pipeline params

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export JAVA_OPTS=$JAVA_OPTS
-LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$url --changeLogFile=$PARAM_CHANGELOG"
+LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$PARAM_URL --changeLogFile=$PARAM_CHANGELOG"
 /liquibase/liquibase --version
 /liquibase/liquibase $LB_PARAMS status --verbose
 /liquibase/liquibase $LB_PARAMS update
