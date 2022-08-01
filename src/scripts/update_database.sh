708dba05edc074880e1aa5771b1f6ec6de4c1d63
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-07-31 22:32:44 -0500

    testing

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $URL_VAR
 export JAVA_OPTS=$JAVA_OPTS
 LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$PARAM_URL --changeLogFile=$PARAM_CHANGELOG"
 /liquibase/liquibase --version
