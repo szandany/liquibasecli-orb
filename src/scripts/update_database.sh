f12e7e6aa24ea08a94f1931e3b060582ef07f828
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2022-08-01 01:07:15 -0500

    Update update_database.sh

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export JAVA_OPTS=$JAVA_OPTS
+echo $URL_VAR
 LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$PARAM_URL --changeLogFile=$PARAM_CHANGELOG"
 /liquibase/liquibase --version
 /liquibase/liquibase $LB_PARAMS status --verbose
