0181a7884625e8c8a321b8babd26056306504821
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-08-04 10:29:35 -0500

    testing

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export JAVA_OPTS=$JAVA_OPTS
-LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$URL_VAR --changeLogFile=$PARAM_CHANGELOG"
-/liquibase/liquibase --version
-/liquibase/liquibase $LB_PARAMS status --verbose
-/liquibase/liquibase $LB_PARAMS update
-/liquibase/liquibase $LB_PARAMS history
+echo $URL_VAR
+# export JAVA_OPTS=$JAVA_OPTS
+# LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$URL_VAR --changeLogFile=$PARAM_CHANGELOG"
+# /liquibase/liquibase --version
+# /liquibase/liquibase $LB_PARAMS status --verbose
+# /liquibase/liquibase $LB_PARAMS update
+# /liquibase/liquibase $LB_PARAMS history
