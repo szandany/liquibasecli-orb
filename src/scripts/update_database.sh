28ceeb3407158d6d66d860b25baa1ae429ee337a
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-08-04 14:23:06 -0500

    update

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # echo $URL_VAR
 # echo $PARAM_CHANGELOG
-export LIQUIBASE_PRO_LICENSE_KEY="$PARAM_LBKEY"
+# export LIQUIBASE_PRO_LICENSE_KEY="$PARAM_LBKEY"
 export JAVA_OPTS=$JAVA_OPTS
 LB_PARAMS="--username=$PARAM_USERNAME --password=$PARAM_PASSWORD --url=$PARAM_URL --changeLogFile=$PARAM_CHANGELOG $LB_ARGS"
 liquibase --version
