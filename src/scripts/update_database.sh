9131cd82551ad431cc30fc28092eeac8965f737d
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-08-06 18:48:54 -0500

    update

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export JAVA_OPTS=$JAVA_OPTS
 liquibase --version
-liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS ${!LB_LOGGING} status --verbose
-liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS ${!LB_LOGGING} update
-liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS ${!LB_LOGGING} history
+liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS ${LB_LOGGING} status --verbose
+liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS ${LB_LOGGING} update
+liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS ${LB_LOGGING} history
 # for f in *;do mv -v "$f" "${f%.*}_${CIRCLE_BUILD_NUM}.${f##*.}";done return | 0
