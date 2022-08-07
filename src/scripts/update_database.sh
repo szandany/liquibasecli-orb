916c18bb0d4b51ef8d7b08ed7643528d0af31f17
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-08-06 20:25:22 -0500

    update

--- src/scripts/update_database.sh
+++ src/scripts/update_database.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export JAVA_OPTS=$JAVA_OPTS
-echo "liquibase.log-file=$LOG_FILE" > liquibase.properties
+$LOG_FILE=$(echo $LOG_FILE | sed 's/\.[^.]*$//')
+echo "liquibase.log-file=$LOG_FILE_${CIRCLE_BUILD_NUM}.log" > liquibase.properties
 echo "liquibase.log-level=$LOG_LEVEL" >> liquibase.properties
 liquibase --version
 liquibase --changeLogFile=$PARAM_CHANGELOG $LB_ARGS status --verbose
